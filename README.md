d1d3233f48d2774bf2ebfe8d77fb956180d26a65
John Lockwood <john@codesolid.com>
2025-04-22 16:19:10 -0400

    Add configuration and app.py needed by spaces

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+---
+title: {{gradio-langgraph}}
+emoji: 🤗
+colorFrom: yellow
+colorTo: orange
+sdk: {{gradio}}
+sdk_version: 5.25.2
+app_file: app.py
+pinned: false
+---
+
 # gradio-langraph
 A Gradio/LangGraph demo hosted on HuggingSpace Spaces
---
+++ app.py
@@ -0,0 +1,13 @@
+### This is just a hello world to get the party started.
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
